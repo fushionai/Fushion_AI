011e56425515e68814e58bce29eb3c0a3ff2daf5
Faysel Abdella <fayselcode@gmail.com>
2024-10-01 07:22:26 +0300

    remove unused variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
-import { Button } from "@nextui-org/react";
 
 import HomePage from "./(pages)/(home)/HomePage";
 
 export default function Home() {
   return (
-    <div className="">
+    <div>
       <HomePage />
     </div>
   );
