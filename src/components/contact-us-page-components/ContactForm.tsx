6cb139ff1ba96bd9d87a52afbb6fb7ee6d56a05f
kiya kebe <kiyakebe799@gmail.com>
2024-11-23 23:58:24 +0300

    ui update

--- src/components/contact-us-page-components/ContactForm.tsx
+++ src/components/contact-us-page-components/ContactForm.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@nextui-org/react";
+
 const ContactForm = () => {
   return (
     <form className="space-y-6 md:w-1/2">
@@ -67,13 +69,9 @@ const ContactForm = () => {
           className="w-full px-3 py-2 border border-gray-100 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
         ></textarea>
       </div>
-
-      <button
-        type="submit"
-        className="w-full px-4 py-2 text-white bg-[#3300FF] rounded-md hover:bg-[#2600CC] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-      >
+      <Button className="w-full px-4 py-2 text-white bg-[#3300FF] rounded-md hover:bg-[#2600CC] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
         Send
-      </button>
+      </Button>
     </form>
   );
 };
