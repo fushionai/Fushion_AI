f91fb1ffa02a58bd3161f07772e4c93f66e667f0
sumeya <sumeya@addissoftware.com>
2024-11-24 21:23:11 +0300

    feat: save contact message in database

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,20 +1,25 @@
+import pool from "@/config/db";
 import { mailOptions, transporter } from "@/config/nodemailer";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const Contact = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const data = req.body;
+
+    // Check if data is provided
     if (!data) {
       return res.status(400).json({ error: "No data provided" });
-    } else if (
-      !data.firstName ||
-      !data.lastName ||
-      !data.email ||
-      !data.message ||
-      !data.company
-    ) {
-      return res.status(400).json({ error: "Missing required fields" });
     }
+
+    //check db connection
+    pool
+      .connect()
+      .then(() => {
+        console.log("Connected to PostgreSQL database");
+      })
+      .catch((err) =>
+        console.error("Error connecting to PostgreSQL database", err)
+      );
 
     // Send email
     try {
@@ -24,14 +29,27 @@ const Contact = async (req: NextApiRequest, res: NextApiResponse) => {
         text: "this is a test string",
         html: `<div> 
         <h1>New Contact Form Submission</h1>
-        <p>First name: ${data.firstName}</p>
-        <p>Last name: ${data.lastName}</p>
-        <p>Email: ${data.email}</p>
-        <p>Phone: ${data.phone || "N/A"}</p>
-        <p>Company: ${data.company}</p>
-        <p>Message: ${data.message}</p>  
+        <p>First name: ${data?.firstName}</p>
+        <p>Last name: ${data?.lastName}</p>
+        <p>Email: ${data?.email}</p>
+        <p>Phone: ${data?.phone || "N/A"}</p>
+        <p>Company: ${data?.company}</p>
+        <p>Message: ${data?.message}</p>  
         </div>`,
       });
+
+      // Store message in database
+      await pool.query(
+        "INSERT INTO messages (first_name, last_name, email, phone, company, message, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, CURRENT_DATE, CURRENT_DATE)",
+        [
+          data.firstName,
+          data?.lastName,
+          data?.email,
+          data?.phone,
+          data?.company,
+          data?.message,
+        ]
+      );
       return res.status(200).json({ message: "Email sent successfully" });
     } catch (error) {
       console.error(error);
