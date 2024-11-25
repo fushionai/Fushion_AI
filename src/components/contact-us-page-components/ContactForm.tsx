35e9c6365dcb73f1ead4d2c1f098316dae41536e
Faysel Abdella <fayselcode@gmail.com>
2024-11-25 13:31:44 +0300

    Update contact us

--- src/components/contact-us-page-components/ContactForm.tsx
+++ src/components/contact-us-page-components/ContactForm.tsx
@@ -11,7 +11,7 @@ const ContactForm = () => {
             name="firstName"
             placeholder="First Name"
             required
-            className="w-full px-3 py-2 border border-gray-100 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 placeholder:font-roboto"
+            className="w-full px-3 py-2 border border-gray-100 rounded-[3px] shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 placeholder:font-roboto placeholder:text-[#77818A] placeholder:text-[18px]"
           />
         </div>
 
@@ -22,7 +22,7 @@ const ContactForm = () => {
             name="lastName"
             placeholder="Last Name"
             required
-            className="w-full px-3 py-2 border border-gray-100 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 placeholder:font-roboto"
+            className="w-full px-3 py-2 border border-gray-100 rounded-[3px] shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 placeholder:font-roboto placeholder:text-[#77818A] placeholder:text-[18px]"
           />
         </div>
 
@@ -33,7 +33,7 @@ const ContactForm = () => {
             name="email"
             placeholder="Email"
             required
-            className="w-full px-3 py-2 border border-gray-100 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 placeholder:font-roboto"
+            className="w-full px-3 py-2 border border-gray-100 rounded-[3px] shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 placeholder:font-roboto placeholder:text-[#77818A] placeholder:text-[18px]"
           />
         </div>
 
@@ -43,7 +43,7 @@ const ContactForm = () => {
             id="phone"
             name="phone"
             placeholder="Phone (Optional)"
-            className="w-full px-3 py-2 border border-gray-100 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 placeholder:font-roboto"
+            className="w-full px-3 py-2 border border-gray-100 rounded-[3px] shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 placeholder:font-roboto placeholder:text-[#77818A] placeholder:text-[18px]"
           />
         </div>
       </div>
@@ -55,7 +55,7 @@ const ContactForm = () => {
           name="company"
           placeholder="Company name"
           required
-          className="w-full px-3 py-2 border border-gray-100 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 placeholder:font-roboto"
+          className="w-full px-3 py-2 border border-gray-100 rounded-[3px] shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 placeholder:font-roboto placeholder:text-[#77818A] placeholder:text-[18px]"
         />
       </div>
 
@@ -66,12 +66,12 @@ const ContactForm = () => {
           required
           placeholder="Message"
           rows={6}
-          className="w-full px-3 py-2 border border-gray-100 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 placeholder:font-roboto"
+          className="w-full px-3 py-2 border border-gray-100 rounded-[3px] shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 placeholder:font-roboto placeholder:text-[#77818A] placeholder:text-[18px]"
         ></textarea>
       </div>
-      <Button className="w-full px-4 py-2 text-white bg-[#3300FF] rounded-md hover:bg-[#2600CC] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 font-roboto font-semibold">
+      <button className="w-full px-4 py-4  text-2xl font-bold text-white bg-[#3300FF] rounded-none hover:bg-[#2600CC]  font-roboto ">
         Send
-      </Button>
+      </button>
     </form>
   );
 };
