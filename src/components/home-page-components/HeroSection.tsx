9364aee4aaf37dc0c809d1c9a3453b03aa842df6
Faysel Abdella <fayselcode@gmail.com>
2024-12-02 15:25:14 +0300

    Update responsive of home page

--- src/components/home-page-components/HeroSection.tsx
+++ src/components/home-page-components/HeroSection.tsx
@@ -21,19 +21,19 @@ const HeroSection = () => {
           <TopNavBar />
         </header>
         <main className="mt-16 px-10 md:mt-24 md:px-32 max-sm:px-6">
-          <h2 className="md:w-[600px] xl:w-[738px] font-ubuntu text-[64px] max-sm:text-[32px] font-bold text-secondaryWhite leading-[73px] max-sm:leading-[48px]">
+          <h2 className="md:w-[600px] xl:w-[738px] font-ubuntu text-[64px] max-sm:text-[32px] max-sm:pr-10 font-bold text-secondaryWhite leading-[73px] max-sm:leading-[48px]">
             Innovating Real Estate with AI Precision
           </h2>
-          <h6 className="sm:mt-10 font-roboto font-normal text-[20px] leading-[30px] text-secondaryWhite">
+          <h6 className="mt-6 sm:mt-10  font-roboto font-normal text-[20px] leading-[30px] text-secondaryWhite">
             Transform real estate with the power of data
           </h6>
           <div className="flex items-center gap-6 mt-20 max-sm:flex-col w-full">
-            <Link href="/our-products">
+            <Link href="/our-products" className="max-sm:w-full ">
               <Button className="h-[60px] w-[185px] max-sm:w-full bg-primaryBlue text-primaryWhite rounded-none font-bold text-2xl leading-[28px] font-roboto">
                 Discover More
               </Button>
             </Link>
-            <Link href="/contact">
+            <Link href="/contact" className="max-sm:w-full ">
               <Button className="h-[60px] w-[185px] max-sm:w-full bg-primaryWhite text-darkBlue rounded-none font-bold text-2xl leading-[28px] font-roboto ">
                 Contact Us
               </Button>
