6a066f2c65950763fa1ffdc0d8bbd5c63c2c9ac1
sumeya <sumeya@addissoftware.com>
2024-12-16 21:37:57 +0300

    Add Google Analytics tracking scripts to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ import { Provider } from "react-redux";
 import { store } from "@/redux/store";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -21,6 +22,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-890ZT6SCEL"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-890ZT6SCEL');
+        `}
+      </Script>
       <body className={`antialiased`}>
         <NextUIProvider>
           <Provider store={store}>
