a7500f73c45cd61345ed852e45bd37c9b47d2e8a
yohanna4 <yohannayim4@gmail.com>
2024-12-31 23:21:59 +0300

    localization setup done

---
+++ src/context/useLanguage.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React, { ReactNode, createContext, useEffect, useState } from "react";
+
+interface LanguageContextType {
+  language: string;
+  changeLanguage: (newLanguage: string) => void;
+}
+
+export const LanguageContext = createContext<LanguageContextType>({
+  language: "en",
+  changeLanguage: () => {},
+});
+interface LanguageProviderProps {
+  children: ReactNode;
+}
+export const LanguageProvider: React.FC<LanguageProviderProps> = ({
+  children,
+}) => {
+  const [language, setLanguage] = useState<string>("en");
+
+  useEffect(() => {
+    if (window != undefined) {
+      const preferredLanguage = localStorage.getItem("preferredLanguage");
+      if (preferredLanguage !== null) {
+        setLanguage(preferredLanguage);
+      } else {
+        setLanguage("en");
+      }
+    }
+  }, []);
+
+  const changeLanguage = (newLanguage: string) => {
+    setLanguage(newLanguage);
+    localStorage.setItem("preferredLanguage", newLanguage);
+  };
+
+  return (
+    <LanguageContext.Provider value={{ language, changeLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
